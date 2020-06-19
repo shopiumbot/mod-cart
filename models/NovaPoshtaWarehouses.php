69a10c5f5c32cc15a89901734dce4e0e5be7c7bc
Andrew <andrew.panix@gmail.com>
2020-06-19 12:36:06 +0300

    init

---
+++ models/NovaPoshtaWarehouses.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace shopium\mod\cart\models;
+
+use Yii;
+use core\components\ActiveRecord;
+
+/**
+ * Class NovaPoshtaCities
+ *
+ * @property float $price
+ * @property float $free_from
+ * @property string $system
+ * @property string $name
+ *
+ * @package shopium\mod\cart\models
+ */
+class NovaPoshtaWarehouses extends ActiveRecord
+{
+    public static function getDb()
+    {
+        return Yii::$app->serverDb;
+    }
+
+    const MODULE_ID = 'cart';
+
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%novaposhta_warehouses}}';
+    }
+
+    public static function find()
+    {
+        return new query\DeliveryQuery(get_called_class());
+    }
+
+}
