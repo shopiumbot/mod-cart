49b1212e3db7cc11228f8532be39c7fde39bc70a
Andrew <andrew.panix@gmail.com>
2020-06-28 09:53:31 +0300

    init

--- views/admin/default/_form.php
+++ views/admin/default/_form.php
@@ -27,7 +27,7 @@ $form = ActiveForm::begin([
         <?=
         $form->field($model, 'status_id')
             ->dropDownList(ArrayHelper::map(OrderStatus::find()->all(), 'id', 'name'))
-            ->hint('Пользователю будет отправлено уведомление об изменение статуса.');
+            ->hint('После заполнение статуса, клиенту будет отправлено уведомление.');
         ?>
         <?=
         $form->field($model, 'payment_id')->dropDownList(ArrayHelper::map(Payment::find()->all(), 'id', 'name'), [
